451eaab4fb5144d3d4c5c3a6ae1b498e8f5192fd
Key Computer <keycomputereducation@gmail.com>
2026-09-05 20:10:57 +0530

    5 sept

--- LinkedList_DSA/SinglyLL.java
+++ LinkedList_DSA/SinglyLL.java
@@ -20,8 +20,8 @@ class LinkedList{
         else{
             newnode.next = this.head ;
             this.head = newnode; 
-            this.size++;
         }
+        this.size++;
     }   
     void insertlast(int elem)
     {
@@ -31,9 +31,7 @@ class LinkedList{
         else
         {
             this.tail.next = newnode;
-            this.tail = newnode;
-            this.size++;
-            
+            this.tail = newnode;            
             //Asssuming tail node is not present 
             // Node temp = this.head;
             // while(temp.next != null) // Stop at last node
@@ -43,6 +41,7 @@ class LinkedList{
             // temp.next = newnode;
 
         }
+        this.size++;
     }
     void insertPos(int elem, int pos)
     {
@@ -87,15 +86,7 @@ class LinkedList{
         //     temp.next= newnode;   
         // }
     }
-    // void insertAfterValue(int elem);
-    // void insertMid(int elem);
-    // void insertSortedOrder(int elem);
-    // void delMide();
-    // void deleteValue // afterValue() 
-    // reverse 
-    // merge 
-    // bubblsort 
-    // split 
+
     void deleteFirst()
     {
         // With Size and Tail 
@@ -176,6 +167,205 @@ class LinkedList{
         }
         System.out.println();
     }
+    void insertAfterValue(int elem, int value) throws NullPointerException
+    {
+        if(this.head == null)
+            throw new NullPointerException();
+        else if (this.head.next== null && this.head.data == value)
+            insertlast(elem);
+        // else if(this.head.next!= null  && this.tail.data == value)
+        //     insertlast(elem);
+        else{
+            Node temp = this.head;  
+            Node newnode = new Node(elem);
+            while(temp != null && temp.data != value)
+            {
+                temp = temp.next; 
+            }
+            if(temp == null){
+                System.out.println("Data not Found - cannot insert value ");
+                return ;
+            }
+            else // 1 2 3 4 
+            {
+                newnode.next = temp.next ;
+                temp.next = newnode;
+                if (temp == this.tail)
+                    tail = newnode;
+            }
+            this.size++;
+        }
+
+    }
+    void insertMid(int elem)
+    {
+        // size / 2 
+        // insert at pos -> pos 1 ?? pos invalid 
+        Node slow = head;
+        Node fast = head ;
+        Node prev = null;
+        while(fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println("Middle Data = " + slow.data);
+        Node newnode = new Node(elem);
+        newnode.next = slow;
+        prev.next= newnode;
+        this.size++;
+    }
+
+    void insertSortedOrder(int elem)
+    {
+        Node newnode = new Node(elem);
+        if (this.head == null)
+            this.head = this.tail = newnode;
+        else if (this.head.data > elem) // insert at begin 
+        {
+            newnode.next= this.head;
+            this.head = newnode;
+        }        
+        else
+        {
+            Node temp = this.head ;
+            while(temp.next != null && temp.next.data < elem )
+                temp = temp.next;
+            if(temp.next == null)
+            {
+                this.tail.next =  newnode;
+                this.tail = newnode;
+            }
+            else
+            {
+                newnode.next = temp.next;
+                temp.next = newnode;
+            }
+        }
+        this.size++;
+    }
+    void delMid()
+    {
+        // size / 2 
+        // delete pos 
+
+        Node prev = null;
+        Node slow = head ;
+        Node fast = head ;
+
+        while(fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next; 
+        }
+        System.out.println("Data  "+ slow.data +" deleted ");
+        prev.next = slow.next; 
+        slow = null;
+        this.size++;
+    }
+    void deleteValue(int elem)  
+    {
+        if (this.head == null)
+            throw new NullPointerException();
+        else if (this.head.data== elem)
+        {
+            deleteFirst();
+        }
+        else
+        {
+            Node temp = head; 
+            while(temp.next != null && temp.next.data != elem)
+            {
+                temp = temp.next;
+
+            } // 1 2 3 4 5 
+            if(temp.next == null)
+            {
+                System.out.println("Data not found for deletion ");
+                return;
+            }
+            else
+            {    // 1 2 3 4
+                System.out.println("Data "+ temp.next.data +" deleted ");
+                temp.next = temp.next.next; 
+                if(temp.next == this.tail)
+                    tail = temp;
+                this.size--;
+            }
+        }
+    }
+    // afterValue()
+    // reverse 
+    void reverse()
+    {
+        Node prev = null;
+        Node curr = this.head ;
+        Node next;
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next = prev; 
+            prev = curr;
+            curr = next ; 
+        }
+        this.head = prev ;
+
+    }
+    void merge(LinkedList obj1 , LinkedList obj2)
+    {
+        if(obj1.head== null)
+            this.head = obj2.head;
+        else if(obj2.head == null)
+            this.head = obj1.head;
+        else
+        {
+            Node temp1 = obj1.head;
+            Node temp2 = obj2.head;
+            while(temp1 != null && temp2 != null)
+            {
+
+                if (temp1.data < temp2.data)
+                {
+                    // System.out.println(temp1.data + " "+temp2.data );
+                    if(this.head == null)
+                        this.head = this.tail = temp1;
+                    else{
+                        this.tail.next = temp1;
+                        this.tail = temp1;
+                    }
+                    temp1 = temp1.next ;
+                }
+
+                else
+                {
+                    if(this.head == null)
+                        this.head = this.tail = temp1;
+                    else{
+                        this.tail.next = temp1;
+                        this.tail = temp1;
+                    }
+                    temp2 = temp2.next;
+                }
+            }
+            if(temp1 != null)
+            {
+                this.tail.next = temp1;
+                this.tail= obj1.tail;
+            }
+                        if(temp1 != null)
+            {
+                this.tail.next = temp1;
+                this.tail= obj1.tail;
+            }
+            this.tail.next= null;
+        }
+        
+
+    } 
+    // bubblsort 
+    // split 
 }
 public class SinglyLL {
     public static void main(String[] args) {
@@ -192,5 +382,40 @@ public class SinglyLL {
         obj.insertlast(700);
         obj.display();
         obj.deletePos(3);
+        obj.insertAfterValue(800, 500);
+        obj.display();
+        obj.insertAfterValue(900, 700);
+        obj.display();
+        obj.insertAfterValue(110,  800);
+        obj.display();
+        obj.insertMid(100);
+        obj.display();
+        obj.delMid();
+        obj.display();
+        obj.deleteValue(100);
+        obj.display();
+        obj.reverse();
+        obj.display();
+        // 
+        System.out.println("Insert in sorted Order ");
+        LinkedList obj2 = new LinkedList();
+        obj2.insertSortedOrder(20);
+        obj2.insertSortedOrder(10);
+        obj2.insertSortedOrder(14);
+        obj2.insertSortedOrder(17);
+        obj2.insertSortedOrder(11);
+        obj2.insertSortedOrder(22);
+        obj2.display();
+        LinkedList obj3 = new LinkedList();
+        obj3.insertSortedOrder(2);
+        obj3.insertSortedOrder(11);
+        obj3.insertSortedOrder(16);
+        obj3.insertSortedOrder(9);
+        obj3.insertSortedOrder(19);
+        obj3.insertSortedOrder(5    );
+        obj3.display();
+        LinkedList obj4 = new LinkedList();
+        obj4.merge(obj2, obj3);
+        obj4.display();
     }
 }
